f239c6b05ea29cd1407d61ae1ae0ca43ccefebcd
monaqa <cmonaqa@gmail.com>
2021-10-10 13:00:32 +0900

    add feature: jump buffer, save colorscheme

--- denops/colordinate/main.ts
+++ denops/colordinate/main.ts
@@ -3,24 +3,21 @@ import * as fn from "https://deno.land/x/denops_std@v2.0.1/function/mod.ts";
 import * as autocmd from "https://deno.land/x/denops_std@v2.0.1/autocmd/mod.ts";
 import {
   ensureObject,
+  ensureString,
+  isArray,
   isObject,
+  isString,
 } from "https://deno.land/x/unknownutil@v1.1.0/mod.ts";
 import * as helper from "https://deno.land/x/denops_std@v2.0.1/helper/mod.ts";
+import { globals } from "https://deno.land/x/denops_std@v2.0.1/variable/mod.ts";
 import {
   parse,
   stringify,
 } from "https://deno.land/std@0.110.0/encoding/yaml.ts";
+import { existsSync } from "https://deno.land/std@0.110.0/fs/mod.ts";
 
 type HighlightName = string;
-type HighlightAttr =
-  | "bold"
-  | "italic"
-  | "reverse"
-  | "standout"
-  | "underline"
-  | "undercurl"
-  | "strikethrough";
-const HIGHLIGHT_ATTRS: HighlightAttr[] = [
+const HIGHLIGHT_ATTRS = [
   "bold",
   "italic",
   "reverse",
@@ -28,7 +25,18 @@ const HIGHLIGHT_ATTRS: HighlightAttr[] = [
   "underline",
   "undercurl",
   "strikethrough",
-];
+] as const;
+type HighlightAttr = typeof HIGHLIGHT_ATTRS[number];
+
+function isHighlightAttr(x: unknown): x is HighlightAttr {
+  return isString(x) && (HIGHLIGHT_ATTRS as unknown as string[]).includes(x);
+}
+
+const YAML_KEY_ORDER = ["color", "style", "links"];
+
+function sortYamlKey(a: string, b: string): number {
+  return YAML_KEY_ORDER.indexOf(a) - YAML_KEY_ORDER.indexOf(b);
+}
 
 // type ColorConfigDict = Record<HighlightName, ColorConfig>;
 type ColorConfig = {
@@ -39,19 +47,50 @@ type ColorConfig = {
 
 function isColorConfig(x: unknown): x is ColorConfig {
   if (!isObject(x)) {
+    console.error("The value of color config must be an object. Actual:");
+    console.error(x);
     return false;
   }
   if ("color" in x) {
-    if (!isObject(x["color"])) return false;
+    if (!isObject(x["color"])) {
+      console.error(
+        "The value corresponding to attribute 'color' must be an object. Actual config:",
+      );
+      console.error(x);
+      return false;
+    }
+  }
+  if ("style" in x) {
+    if (!isArray(x["style"], isHighlightAttr)) {
+      console.error(
+        "The value corresponding to attribute 'style' must be a attribute array.",
+      );
+      console.error(
+        `Possible attributes: [${HIGHLIGHT_ATTRS.join(", ")}]. Actual config:`,
+      );
+      console.error(x);
+      return false;
+    }
+  }
+  if ("links" in x) {
+    if (!isArray(x["links"], isString)) {
+      console.error(
+        "The value corresponding to attribute 'links' must be a string array. Actual config:",
+      );
+      console.error(x);
+      return false;
+    }
   }
   return true;
 }
 
 class ColorConfigDict {
   record: Record<HighlightName, ColorConfig>;
+  matchIds: number[];
 
   constructor(record: Record<HighlightName, ColorConfig>) {
     this.record = record;
+    this.matchIds = [];
   }
 
   static parse(content: string): ColorConfigDict {
@@ -83,18 +122,22 @@ class ColorConfigDict {
   }
 
   toYaml(): string {
-    return stringify(this.record, { skipInvalid: true });
+    return stringify(this.record, { skipInvalid: true, sortKeys: sortYamlKey });
   }
 
   static async getCurrentConfig(denops: Denops): Promise<ColorConfigDict> {
-    let synId = 1;
+    let synId = 0;
     const record: Record<string, ColorConfig> = {};
     while (true) {
+      synId += 1;
       const transId = await fn.synIDtrans(denops, synId) as number;
       if (transId == 0) {
         break;
       }
       const name = await fn.synIDattr(denops, synId, "name", "gui") as string;
+      if (name == "") {
+        continue;
+      }
 
       if (synId == transId) {
         if (record[name] == undefined) {
@@ -148,10 +191,17 @@ class ColorConfigDict {
           record[transName]["links"]?.push(name);
         }
       }
-
-      synId += 1;
     }
     return new ColorConfigDict(record);
+  }
+
+  async addHighlights(buffer: ColordinateBuffer) {
+    await Promise.all(
+      this.matchIds.map((id) => fn.matchdelete(buffer.denops, id)),
+    );
+    this.matchIds = await Promise.all(
+      Object.keys(this.record).map((name) => buffer.addHighlight(name)),
+    );
   }
 }
 
@@ -178,23 +228,34 @@ class ColordinateBuffer {
       await fn.fnameescape(denops, name) as string,
     );
 
-    await fn.setbufvar(denops, id, "&buftype", "acfile");
-    await fn.setbufvar(denops, id, "&bufhidden", "wipe");
+    await fn.setbufvar(denops, id, "&buftype", "nowrite");
     await fn.setbufvar(denops, id, "&swapfile", 0);
     await fn.setbufvar(denops, id, "&backup", 0);
     await fn.setbufvar(denops, id, "&foldenable", 1);
-    await fn.setbufvar(denops, id, "&foldmethod", "indent");
+    // await fn.setbufvar(denops, id, "&foldmethod", "indent");
+    await fn.setbufvar(denops, id, "&foldmethod", "expr");
+    await fn.setbufvar(
+      denops,
+      id,
+      "&foldexpr",
+      "getline(v:lnum)[:5]=='    - '",
+    );
     await fn.setbufvar(denops, id, "&foldminlines", 3);
-    await fn.setbufvar(denops, id, "&foldlevelstart", 2);
+    await fn.setbufvar(denops, id, "&foldlevelstart", 0);
     await fn.setbufvar(denops, id, "&filetype", "yaml");
     await fn.setbufvar(denops, id, "&syntax", "OFF");
 
     await autocmd.group(denops, "colordinate", (helper) => {
       helper.remove("*", "<buffer>");
       helper.define(
-        "BufWriteCmd",
+        ["BufReadPost", "BufWinEnter", "TextChanged"],
         "<buffer>",
         `call denops#notify("${denops.name}", "reflect", [])`,
+      );
+      helper.define(
+        "BufWriteCmd",
+        "<buffer>",
+        `setlocal nomodified`,
       );
     });
 
@@ -215,6 +276,10 @@ class ColordinateBuffer {
     await fn.deletebufline(this.denops, this.id, 1, "$");
     await fn.appendbufline(this.denops, this.id, 0, lines);
   }
+
+  async addHighlight(name: string): Promise<number> {
+    return await fn.matchadd(this.denops, name, `\\<${name}\\>`) as number;
+  }
 }
 
 export async function main(denops: Denops): Promise<void> {
@@ -234,26 +299,93 @@ export async function main(denops: Denops): Promise<void> {
       await helper.execute(
         denops,
         `
-      if exists('syntax_on')
-        syntax reset
-      endif
-      let g:colors_name = 'colordinate'
-      `,
+        if exists('syntax_on')
+          syntax reset
+        endif
+        let g:colors_name = 'colordinate'
+        `,
       );
       await helper.execute(denops, conf.toScript());
-      await buffer.resetModified();
+      await conf.addHighlights(buffer);
     },
 
     async load() {
       buffer = await ColordinateBuffer.init(denops, "[Colordinate]");
       const conf = await ColorConfigDict.getCurrentConfig(denops);
       await buffer.setText(conf.toYaml());
-      await buffer.resetModified();
+    },
+
+    async jump() {
+      const pos = await fn.getcurpos(denops);
+      const synId = await fn.synID(denops, pos[1], pos[2], false) as number;
+      const synName = await fn.synIDattr(denops, synId, "name") as string;
+
+      console.log(synName);
+
+      if (buffer == null) {
+        buffer = await ColordinateBuffer.init(denops, "[Colordinate]");
+        const conf = await ColorConfigDict.getCurrentConfig(denops);
+        await buffer.setText(conf.toYaml());
+      } else {
+        const winnr = await fn.winbufnr(
+          denops,
+          await fn.bufnr(denops, "\\[Colordinate\\]") as number,
+        ) as number;
+        if (winnr > 0) {
+          const winid = await fn.win_getid(denops, winnr) as number;
+          await fn.win_gotoid(denops, winid);
+        } else {
+          await helper.execute(denops, "sbuffer \\[Colordinate]");
+        }
+      }
+
+      await fn.search(denops, `\\<${synName}\\>`, "w");
+    },
+
+    async save(csname: unknown) {
+      ensureString(csname);
+      if (buffer == null) {
+        throw new Error(
+          "Buffer [Colordinate] is not found. First you should run :ColordinateLoad.",
+        );
+      }
+
+      const fpath = await globals.get<string>(denops, "colordinate_save_path");
+      const fname = (fpath == null)
+        ? `${csname}.vim`
+        : `${fpath}/${csname}.vim`;
+      const text = (await buffer.getLines()).join("\n");
+      const conf = ColorConfigDict.parse(text);
+      let content = [
+        `" Generated by colordinate.vim (https://github.com/monaqa/colordinate.vim)`,
+        `if exists('syntax_on')`,
+        `  syntax reset`,
+        `endif`,
+        `let g:colors_name = '${csname}'`,
+        "",
+        "",
+      ].join("\n");
+
+      content += conf.toScript();
+      if (
+        !existsSync(fname) ||
+        await fn.confirm(
+            denops,
+            `The file ${fname} already exists. Overwrite it?`,
+            "&Yes\n&No",
+          ) == 1
+      ) {
+        await Deno.writeTextFile(fname, content);
+      }
     },
   };
 
   await helper.execute(
     denops,
-    `command! ColordinateLoad call denops#request("${denops.name}", "load", [])`,
+    `
+    command! ColordinateLoad call denops#request("${denops.name}", "load", [])
+    command! ColordinateJump call denops#request("${denops.name}", "jump", [])
+    command! -nargs=1 ColordinateSave call denops#request("${denops.name}", "save", [<q-args>])
+    `,
   );
 }
